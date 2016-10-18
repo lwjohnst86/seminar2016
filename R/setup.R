78011bd48d3d08ff3c9d8f758755ad995485bc87
Luke W Johnston <lwjohnst@gmail.com>
2016-10-18 16:40:04 -0400

    Updated setup function with knitr options

--- R/setup.R
+++ R/setup.R
@@ -17,16 +17,10 @@
 set_options <- function() {
     # Set the options here for individual packages
 
-    # For tables (uncomment below if using pander)
-#     pander::panderOptions('table.split.table', Inf)
-#     pander::panderOptions('table.style', 'rmarkdown')
-#     pander::panderOptions('table.alignment.default',
-#                   function(df)
-#                       ifelse(sapply(df, is.numeric), 'center', 'left'))
-
     # For the document (knitr)
     knitr::opts_chunk$set(
-        warning = FALSE, message = FALSE, collapse = TRUE
-    )
+        warning = FALSE, message = FALSE, collapse = TRUE,
+        fig.path = '../img/', echo = FALSE
+        )
 }
 
