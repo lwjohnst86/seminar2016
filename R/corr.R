8d087825a554cdd015d936affa24cb89e0f81725
Luke W Johnston <lwjohnst@gmail.com>
2016-10-18 20:22:38 -0400

    Fixed up corr function so that heatmap matrix works

--- R/corr.R
+++ R/corr.R
@@ -13,15 +13,22 @@ analyze_corr <-
     function(data = project_data,
              x = tg_pct) {
 
+        name_order <- substr(x, nchar(x), nchar(x))
+        name_order <- as.integer(name_order)
+        name_order <- x[order(name_order)]
+
     data %>%
         dplyr::filter(VN == 0) %>%
+        dplyr::select_(.dots = x) %>%
         mason::design('cor') %>%
-        mason::add_settings(method = 'pearson', use = 'complete.obs') %>%
+        mason::add_settings(method = 'pearson', use = 'complete.obs', hclust.order = TRUE) %>%
         mason::add_variables('xvars', x) %>%
         mason::construct() %>%
         mason::scrub() %>%
         mason::polish_renaming(renaming_fats, 'Vars2') %>%
-        mason::polish_renaming(renaming_fats, 'Vars1')
+        mason::polish_renaming(renaming_fats, 'Vars1') %>%
+        dplyr::mutate(Vars2 = factor(Vars2, unique(Vars2)),
+                      Vars1 = factor(Vars1, unique(Vars1)))
     }
 
 # Plotting ----------------------------------------------------------------
